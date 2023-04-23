30d3bdedb0272ecf5ecfe03026b89e46ab467adc
Antonio Claudio <claudio1419teixeira2020@gmail.com>
2023-04-23 16:22:15 -0300

    teste

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-from menu import Menu
+# from menu import Menu
 
-def init():
-    Menu.menu()
+# def init():
+#     Menu.menu()
 
-    option = input("Sua opção: ")
+#     option = input("Sua opção: ")
 
-    try:
-        option = int(option)
-    except ValueError:
-        print("Opção inválida. Por favor, digite um número válido.")
+#     try:
+#         option = int(option)
+#     except ValueError:
+#         print("Opção inválida. Por favor, digite um número válido.")
 
-    if option == 1:
-        Menu.motorista_menu()
-    elif option == 2:
-        Menu.veiculo_menu()
-    elif option == 3:
-        Menu.viagem_menu()
-    elif option == 4:
-        pass
-    elif option == 5:
-        pass
-    elif option == 6:
-        pass
-    else:
-        print('[ERROR] Opção inválida x/')
+#     if option == 1:
+#         Menu.motorista_menu()
+#     elif option == 2:
+#         Menu.veiculo_menu()
+#     elif option == 3:
+#         Menu.viagem_menu()
+#     elif option == 4:
+#         pass
+#     elif option == 5:
+#         pass
+#     elif option == 6:
+#         pass
+#     else:
+#         print('[ERROR] Opção inválida x/')
 
-init()
+# init()
