807d20a697b3509f0eb2d027d208c78bc6814ef1
Pedro <pedrohenrique2.0583@gmail.com>
2023-04-23 16:33:20 -0300

    separação de classes

--- classes/admin.py
+++ classes/admin.py
@@ -1,3 +1,4 @@
+from driver import driver
 class admin:
     def registerDriver(self,driversDb ):
 
@@ -20,10 +21,3 @@ class admin:
 
     def deleteDriver(self,):
         pass
-class driver:
-    def __init__(self, name = "Nome não informado", cpf = "CPF não informado", rg = "RG não informado", cnh = "CNH não informado") -> None:
-        
-        self.name = name
-        self.CPF  = cpf
-        self.RG = rg
-        self.CNH = cnh
---
+++ classes/driver.py
@@ -0,0 +1,8 @@
+
+class driver:
+    def __init__(self, name = "Nome não informado", cpf = "CPF não informado", rg = "RG não informado", cnh = "CNH não informado") -> None:
+        
+        self.name = name
+        self.CPF  = cpf
+        self.RG = rg
+        self.CNH = cnh
