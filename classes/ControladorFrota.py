5932ece15e3890346e80722210f6046dd1ac92e8
Kallinycele <94210840+Kallinycele@users.noreply.github.com>
2023-04-27 19:48:33 -0300

    ControladorFrota

--- classes/ControladorFrota.py
+++ classes/ControladorFrota.py
@@ -1,17 +1,24 @@
 from Driver import Driver 
 from Veicle import Veicle
 from Admin import Admin
+from Suply import Suply
+from Maintainance import Maintainance
 
 class ControladorFrota:
     def __init__(self, admin: Admin):
         self.disponivel = [i for i in range(admin.veicle)]
         self.indisponivel = []
-    #Veicular motorista com o veiculo 
+     
     def alocarVeicleByDriver(driver: Driver, veicle: Veicle):
         pass
 
-    def programarManutencao():
-        pass
+    def programarManutencao(self, maintainanceDb, veicleDb):
+        veicleData = self.searchVeicle(str(input("Digite a placa do veiculo: ")), veicleDb)
+        date = str(input("Digite a data da manutenção: "))
+        mType = str(input("Digite o tipo da manutanção: "))
+        cost = float("Digite o valor da manutenção: ")
+        
+        maintainanceDb.append(Maintainance(veicleData, date, mType, cost))
 
     def programarAbastecimento():
         pass
